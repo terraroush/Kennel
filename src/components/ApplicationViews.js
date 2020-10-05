e677afd30c6411fadd908f8b1d50c748ab324dc1
Terra Roush <terra.roush@gmail.com>
2020-10-05 15:25:03 -0500

    adjusted animalCard to include owner name; finished location provider and location list components and adjusted locationCard to display the correct info. Imported locationProvider into applicationViews to give access where needed to child components

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,32 +1,33 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
-import { LocationCard } from "./location/LocationCard"
 import { AnimalList } from "./animal/AnimalList"
 import { CustomerList } from "./customer/CustomerList"
 import { EmployeeList } from "./employee/EmployeeList"
+import { LocationList } from "./location/LocationList"
 import { AnimalProvider } from "./animal/AnimalProvider"
 import { CustomerProvider } from "./customer/CustomerProvider"
 import { EmployeeProvider } from "./employee/EmployeeProvider"
+import { LocationProvider } from "./location/LocationProvider"
 
 export const ApplicationViews = () => {
     return (
         <>
-            {/* Render the location list when http://localhost:3000/ */}
             <Route exact path="/">
                 <Home />
             </Route>
 
-            {/* Render the animal list when http://localhost:3000/animals */}
             <AnimalProvider>
                 <Route path="/animals">
                     <AnimalList />
                 </Route>
             </AnimalProvider>
 
-            <Route path="/locations">
-                <LocationCard />
-            </Route>
+            <LocationProvider>
+                <Route path="/locations">
+                    <LocationList />
+                </Route>
+            </LocationProvider>
 
             <CustomerProvider>
                 <Route path="/customers">
