504eb4015659f854ad33b04edcd49911df7828c3
Terra Roush <terra.roush@gmail.com>
2020-10-07 14:25:02 -0500

    refactored animalDetails.js

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -7,27 +7,28 @@ export const AnimalDetail = () => {
   const { getAnimalById, releaseAnimal } = useContext(AnimalContext);
 
   const [animal, setAnimal] = useState({});
-  const [location, setLocation] = useState({});
-  const [customer, setCustomer] = useState({});
+//   const [location, setLocation] = useState({});
+//   const [customer, setCustomer] = useState({});
 
   const { animalId } = useParams();
   const history = useHistory();
 
   useEffect(() => {
     console.log("useEffect", animalId);
-    getAnimalById(animalId).then((response) => {
+    getAnimalById(animalId)
+    .then((response) => {
       setAnimal(response);
-      setLocation(response.location);
-      setCustomer(response.customer);
+    //   setLocation(response.location);
+    //   setCustomer(response.customer);
     });
   }, []);
 
   return (
     <section className="animal">
-      <h3 className="animal__name">{animal.name}</h3>
-      <div className="animal__breed">{animal.breed}</div>
-      <div className="animal__location">Location: {location.name}</div>
-      <div className="animal__owner">Customer: {customer.name}</div>
+      <h3 className="animal__name">{animal?.name}</h3>
+      <div className="animal__breed">{animal?.breed}</div>
+      <div className="animal__location">Location: {animal?.location.name}</div>
+      <div className="animal__owner">Customer: {animal?.customer.name}</div>
       <button
         onClick={() => {
           releaseAnimal(animal.id).then(() => {
